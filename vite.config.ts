114c380fdbe34b1b69d3a081dc941c83f82d4c6c
Gustavo <gujtrafego@gmai.com>
2026-03-05 09:34:03 -0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/",
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: { overlay: false },
-  },
   plugins: [react()],
   resolve: {
     alias: {
